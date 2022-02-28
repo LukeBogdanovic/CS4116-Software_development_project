c3033a02eb9d6f44f2de4cfc938ef4aa05bda459
jackconryan <58440324+jackconryan@users.noreply.github.com>
2022-02-28 23:59:47 +0000

    CS4116-27 Plan more Database Tables

--- sample.sql
+++ sample.sql
@@ -74,8 +74,18 @@ SET
     `Surname` varchar(26) NOT NULL,
     `Password` varchar(256) NOT NULL COMMENT 'See video for information on how to encrypt password BEFORE storing it. Never store the user''s actual password.'
     `Email` varchar(52) NOT NULL
+    ``
+    ``
   ) ENGINE = InnoDB DEFAULT CHARSET = latin1 COMMENT = 'Store personal information about the user. ';
---
+-- --------------------------------------------------------
+  --
+  -- Table structure for table `SecurityAnswers`
+  --
+  CREATE TABLE `SecurityAnswers` (
+    `UserID` int(11) NOT NULL,
+    `SecurityAnswer1` varchar(52) NOT NULL,
+    `SecurityAnswer2` varchar(52) NOT NULL,
+  ) ENGINE = InnoDB DEFAULT CHARSET = latin1 COMMENT = 'Store account recovery answers for each user'
   -- Indexes for dumped tables
   --
   --
@@ -133,6 +143,14 @@ ADD
   CONSTRAINT `Connections_ibfk_1` FOREIGN KEY (`userID1`) REFERENCES `user` (`UserID`),
 ADD
   CONSTRAINT `Connections_ibfk_2` FOREIGN KEY (`userID2`) REFERENCES `user` (`UserID`);
+--
+  -- Constraints for table `SecurityAnswers`
+  --
+ALTER TABLE
+  `SecurityAnswers`
+ADD
+--
+  CONSTRAINT `SecurityAnswers_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`),
 --
   -- Constraints for table `Interests`
   --
