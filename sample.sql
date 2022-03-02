0a10397a044dbc529447fcaa82c149af2919471d
jackconryan <58440324+jackconryan@users.noreply.github.com>
2022-03-02 16:34:45 +0000

    CS4116-27 begun changing foreign keys, primary keys, unique autoincrement etc into table creation

--- sample.sql
+++ sample.sql
@@ -22,19 +22,23 @@ SET
   -- Table structure for table `AvailableInterests`
   --
   CREATE TABLE `AvailableInterests` (
-    `InterestID` int(3) NOT NULL,
-    `InterestName` varchar(26) NOT NULL COMMENT 'The name of the interest'
+    `InterestID` int(3) NOT NULL AUTO_INCREMENT,
+    `InterestName` varchar(26) NOT NULL COMMENT 'The name of the interest',
+    PRIMARY KEY (InterestID)
   ) ENGINE = InnoDB DEFAULT CHARSET = latin1 COMMENT = 'Show a list of available interests for registration/search';
 -- --------------------------------------------------------
   --
   -- Table structure for table `Connections`
   --
   CREATE TABLE `Connections` (
-    `ConnectionID` int(11) NOT NULL,
+    `ConnectionID` int(11) NOT NULL AUTO_INCREMENT,
     `userID1` int(11) NOT NULL COMMENT 'Which user initiated the connection?',
     `userID2` int(11) NOT NULL COMMENT 'Which user received the connection',
     `ConnectionDate` date NOT NULL COMMENT 'When was the connection made?',
-    `Superlike` binary(1) NOT NULL
+    `Superlike` binary(1) NOT NULL,
+    PRIMARY KEY (ConnectionID),
+    FOREIGN KEY (userID1) REFERENCES user(UserID),
+    FOREIGN KEY (userID2) REFERENCES user(UserID)
   ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
 -- --------------------------------------------------------
   --
@@ -42,7 +46,9 @@ SET
   --
   CREATE TABLE `Interests` (
     `UserID` int(11) NOT NULL COMMENT 'Which user is this?',
-    `InterestID` int(3) NOT NULL COMMENT 'Which interest do they have?'
+    `InterestID` int(3) NOT NULL COMMENT 'Which interest do they have?',
+    FOREIGN KEY (UserID) REFERENCES user(UserID),
+    FOREIGN KEY (InterestID) REFERENCES AvailableInterests(InterestID)
   ) ENGINE = InnoDB DEFAULT CHARSET = latin1 COMMENT = 'Interests of ALL users';
 -- --------------------------------------------------------
   --
@@ -73,45 +79,30 @@ SET
   -- Table structure for table `user`
   --
   CREATE TABLE `user` (
-    `UserID` int(11) NOT NULL,
+    `UserID` int(11) NOT NULL AUTO_INCREMENT,
     `Handle` varchar(26) NOT NULL,
     `Firstname` varchar(26) NOT NULL,
     `Surname` varchar(26) NOT NULL,
     `Password` varchar(256) NOT NULL COMMENT 'See video for information on how to encrypt password BEFORE storing it. Never store the user''s actual password.',
     `Email` varchar(52) NOT NULL,
     `SecurityQuestion1` enum('Q1', 'Q2', 'Q3', 'Q4', 'Q5') NOT NULL COMMENT 'Users select which security question they are answering need to decide on these',
-    `SecurityQuestion2` enum('Q1', 'Q2', 'Q3', 'Q4', 'Q5') NOT NULL COMMENT 'Users select which security question they are answering'
+    `SecurityQuestion2` enum('Q1', 'Q2', 'Q3', 'Q4', 'Q5') NOT NULL COMMENT 'Users select which security question they are answering',
+    PRIMARY KEY (UserID),
+    UNIQUE (Email),
+    UNIQUE (Handle)
   ) ENGINE = InnoDB DEFAULT CHARSET = latin1 COMMENT = 'Store personal information about the user. ';
---
-  -- AUTO_INCREMENT values for tables
-  --
-Alter Table
-  `AvailableInterests` change `InterestID` `InterestID` int(3) NOT NULL AUTO_INCREMENT;
-ALTER TABLE
-  `Connections` change `ConnectionID` `ConnectionID` int(11) NOT NULL AUTO_INCREMENT;
-ALTER Table
-  `user` change `UserID` `UserID` int(11) NOT NULL AUTO_INCREMENT;
---
-  -- Add Uniques to user table
-  --
-ALTER TABLE
-  `user`
-ADD
-  UNIQUE (Handle);
-ALTER TABLE
-  `user`
-ADD
-  UNIQUE (Email);
---
   -- --------------------------------------------------------
   --
   -- Table structure for table `Reports`
   --
-  CREATE TABLE `Report` (
+  CREATE TABLE `Reports` (
     `UserID` int(11) NOT NULL,
-    `ReportID` int(11) NOT NULL,
+    `ReportID` int(11) NOT NULL AUTO_INCREMENT,
     `ReportReason` enum('Harassment','Disrespectful behaviour','Hate Speech','Catfish','Bot account')
     `ReporterID` int(11) NOT NULL,
+    PRIMARY KEY (ReportID),
+    FOREIGN KEY (UserID) REFERENCES user(UserID)
+    FOREIGN KEY (ReporterID) REFERENCES user(UserID)
   ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
 -- --------------------------------------------------------
   --
@@ -120,7 +111,8 @@ ADD
   CREATE TABLE `SecurityAnswers` (
     `UserID` int(11) NOT NULL,
     `SecurityAnswer1` varchar(256) NOT NULL,
-    `SecurityAnswer2` varchar(256) NOT NULL
+    `SecurityAnswer2` varchar(256) NOT NULL,
+    FOREIGN KEY (userID) REFERENCES user(userID)
   ) ENGINE = InnoDB DEFAULT CHARSET = latin1 COMMENT = 'Store account recovery answers for each user';
 -- Indexes for dumped tables
   --
@@ -143,6 +135,13 @@ ADD
 ADD
   KEY `userID2` (`userID2`);
 --
+  -- Indexes for table `SecurityAnswers`
+  --
+ALTER TABLE
+  `SecurityAnswers`
+ADD
+  KEY `userID` (`userID`),
+  --
   -- Indexes for table `Interests`
   --
 ALTER TABLE
@@ -170,15 +169,6 @@ ADD
 --
   -- Constraints for dumped tables
   --
-  --
-  -- Constraints for table `Connections`
-  --
-ALTER TABLE
-  `Connections`
-ADD
-  CONSTRAINT `Connections_ibfk_1` FOREIGN KEY (`userID1`) REFERENCES `user` (`UserID`),
-ADD
-  CONSTRAINT `Connections_ibfk_2` FOREIGN KEY (`userID2`) REFERENCES `user` (`UserID`);
 --
   -- Constraints for table `SecurityAnswers`
   --
