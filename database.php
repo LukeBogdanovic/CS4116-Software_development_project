f07cc8366ca4bd24b0cbd121fa8f8ee274dca57a
Luke Bogdanovic <19232691@studentmail.ul.ie>
2022-03-13 14:10:39 +0000

    CS4116-40 Moving console_log php function to utils.php

--- database.php
+++ database.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Database Connection script; Used for connecting to the database
  * Tests the connection to the database and checks for connection error codes
@@ -12,21 +13,4 @@ $DATABASE_NAME = 'epiz_31123825_group13';
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
     exit('Failed to connect to db: ' . mysqli_connect_error());
-} else {
-    console_log('Connected to Database: ' . $DATABASE_NAME);
-}
-
-/**
- * Logs output param provided to the function to the developer console of the browser in use 
- * @param $output String Output to be printed
- * @param $with_script_tags boolean Selector for script html tags
- * @return null
- */
-function console_log($output, $with_script_tags  = true)
-{
-    $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . ');';
-    if ($with_script_tags) {
-        $js_code = '<script>' . $js_code . '</script>';
-    }
-    echo $js_code;
 }
--- utils.php
+++ utils.php
@@ -37,6 +37,17 @@ function validate_password($password, $repeat_password)
     return false;
 }
 
-
-
-echo hash_password("Appleseed12@?");
+/**
+ * Logs output param provided to the function to the developer console of the browser in use 
+ * @param $output String Output to be printed
+ * @param $with_script_tags boolean Selector for script html tags
+ * @return null
+ */
+function console_log($output, $with_script_tags  = true)
+{
+    $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . ');';
+    if ($with_script_tags) {
+        $js_code = '<script>' . $js_code . '</script>';
+    }
+    echo $js_code;
+}
