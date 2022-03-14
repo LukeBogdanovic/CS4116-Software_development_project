b6ad4af84b4e458338b72675b4d3878ad7651a2d
adam <19258356@studentmail.ul.ie>
2022-03-14 15:26:38 +0000

    CS4116-56 Added code & images for Help Page

---
+++ help.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Help</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="http://group13.epizy.com/css/help.css">
+        <style>
+            body {
+                height: 100%;
+                margin: 0;
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                background-image: linear-gradient(to bottom right, rgb(192, 53, 99), rgb(194, 139, 38));
+            }
+        </style>
+
+    </head>
+
+    <body>
+
+        <div>
+
+            <p class="heading"> Help: </p>
+
+            <img src="logo_v1.PNG" class="img1" alt="logo">
+
+            <img src="1619563.png" class="img2" alt="Text box" width="300" height="300">
+            <pre class="text-text"> --->Please contact us via text: 
+                123456789 </pre>
+
+            <img src="aticon.png" class="img3" alt="at icon" width="300" height="300">
+            <pre class="email-text"> --->Please contact us via email: 
+                gragodeo@gmail.com </pre>
+
+        </div>
+
+    </body>
+
+</html>
