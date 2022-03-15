91109babb44718bdaaf84ecc85335d64dc30afa0
adam <19258356@studentmail.ul.ie>
2022-03-15 22:38:14 +0000

    Made small change to footer and help files

--- help.php
+++ help.php
@@ -4,36 +4,41 @@
     <head>
         <title>Help</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="utf-8">
         <link rel="stylesheet" type="text/css" href="http://group13.epizy.com/css/help.css">
-        <style>
-            body {
-                height: 100%;
-                margin: 0;
-                background-repeat: no-repeat;
-                background-attachment: fixed;
-                background-image: linear-gradient(to bottom right, rgb(192, 53, 99), rgb(194, 139, 38));
-            }
-        </style>
-
+        <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
     </head>
 
     <body>
-
+        
+        <?php
+            require "navbar.php";
+        ?>
+        
         <div>
+            <div>
+                <p class="heading"> Help: </p>
+                <img src="logo_v1.PNG" class="img1" alt="logo">
+            </div>
 
-            <p class="heading"> Help: </p>
+            <div>
+                <img src="1619563.png" class="img2" alt="Text box" width="300" height="300">
+                <div>
+                    <p class="text-text"> --->Please contact us via text: 123456789 </p>
+                </div>
+            </div>
 
-            <img src="logo_v1.PNG" class="img1" alt="logo">
-
-            <img src="1619563.png" class="img2" alt="Text box" width="300" height="300">
-            <pre class="text-text"> --->Please contact us via text: 
-                123456789 </pre>
-
-            <img src="aticon.png" class="img3" alt="at icon" width="300" height="300">
-            <pre class="email-text"> --->Please contact us via email: 
-                gragodeo@gmail.com </pre>
-
+            <div>
+                <img src="aticon.png" class="img3" alt="at icon" width="300" height="300">
+                <div>
+                    <p class="email-text"> --->Please contact us via email: gragodeo@gmail.com </p>
+                </div>
+            </div>
         </div>
+
+        <?php
+            require "footer.php";
+        ?>
 
     </body>
 
