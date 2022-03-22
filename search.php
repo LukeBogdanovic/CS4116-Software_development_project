069e78fbf266ad00e9ba788e7279cc84db29127a
jackconryan <58440324+jackconryan@users.noreply.github.com>
2022-03-22 19:16:52 +0000

    CS4116-59 Changed nav to redirect to search page, created search page wih template result

---
+++ search.php
@@ -0,0 +1,72 @@
+<?php
+session_start();
+/* Checking if the user is not logged in
+ * If user is not logged in: the user is redirected to the login page and the script exits
+ */
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+    header("location: login.php");
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/utils.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <title>Home</title>
+</head>
+
+<body class="d-flex flex-column min-vh-100">
+
+    <?php
+    require_once "includes/navbar.php";
+    ?>
+    <div>
+        <div class="container py-5 h-5">
+            <form onsubmit="getSearchResults(event);" method="POST" id="search">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search" name="search">
+                    <div class="input-group-btn">
+                        <button class="btn btn-default" type="submit">Search</i></button>
+                    </div>
+                </div>
+            </form> 
+        </div>
+        <div class="row row-cols-1 row-cols-xs-2 row-cols-sm-2 row-cols-lg-4 g-3">
+            <div class="col">
+                <div class="card h-100 shadow-sm"> <img src="assets/images/logo.PNG" class="card-img-top" alt="Profile Picture">
+                    <div class="card-body">
+                        <h5 class="card-title">Name, Age</h5>
+                        <p class="card-content">Bio</p>
+                        <div class="text-center my-4"> <a class="btn btn-dark">View Profile</a> </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <template>
+            <div class="col">
+                <div class="card h-100 shadow-sm"> <img src="assets/images/logo.PNG" class="card-img-top" alt="Profile Picture">
+                    <div class="card-body">
+                        <h5 class="card-title">Name, Age</h5>
+                        <p class="card-content">Bio</p>
+                        <div class="text-center my-4"> <a class="btn btn-dark">View Profile</a> </div>
+                    </div>
+                </div>
+            </div>
+        </template>
+        
+    </div>
+    <?php
+    require_once "includes/footer.php"
+    ?>
+
+</body>
+
+</html>
