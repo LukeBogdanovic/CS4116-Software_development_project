3cc27e484684d840505657650caa14f3faa23206
adam <19258356@studentmail.ul.ie>
2022-03-22 18:29:18 +0000

    CS4116-54 Added connections page

---
+++ connections.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+  
+    <head>
+        <title>Connections</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="css/utils.css">
+        <link rel="stylesheet" type="text/css" href="css/connections.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    </head>
+
+    <body>
+
+        <?php
+            require "navbar.php";
+        ?>
+
+        <main>
+            <div class="container-fluid bg-trasparent my-4 p-3" style="position: relative;">
+                
+                <div class="row row-cols-1 row-cols-xs-2 row-cols-sm-2 row-cols-lg-4 g-3">
+                    
+                    <div class="col">
+                        <div class="card h-100 shadow-sm"> <img src="profile_pic.png" class="card-img-top" alt="Profile Picture">
+                            <div class="card-body">
+                                <h5 class="card-title">Name, Age</h5>
+                                <p class="card-content">Bio</p>
+                                <div class="text-center my-4"> <a class="btn btn-dark">View Profile</a> </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col">
+                        <div class="card h-100 shadow-sm"> <img src="profile_pic.png" class="card-img-top" alt="Profile Picture">
+                            <div class="card-body">
+                                <h5 class="card-title">Name, Age</h5>
+                                <p class="card-content">Bio</p>
+                                <div class="text-center my-4"> <a class="btn btn-dark">View Profile</a> </div>
+                            </div>
+                        </div>
+                    </div>
+            
+                    <div class="col">
+                        <div class="card h-100 shadow-sm"> <img src="profile_pic.png" class="card-img-top" alt="Profile Picture">
+                            <div class="card-body">
+                                <h5 class="card-title">Name, Age</h5>
+                                <p class="card-content">Bio</p>
+                                <div class="text-center my-4"> <a class="btn btn-dark">View Profile</a> </div>
+                            </div>
+                        </div>
+                    </div>
+            
+                    <div class="col">
+                        <div class="card h-100 shadow-sm"> <img src="profile_pic.png" class="card-img-top" alt="Profile Picture">
+                            <div class="card-body">
+                                <h5 class="card-title">Name, Age</h5>
+                                <p class="card-content">Bio</p>
+                                <div class="text-center my-4"> <a class="btn btn-dark">View Profile</a> </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </main>
+
+        <?php
+            require "footer.php";
+        ?>
+
+    </body>
+
+</html>
