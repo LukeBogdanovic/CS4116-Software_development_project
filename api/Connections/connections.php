5ace6f54e27b309f73b43b2cb7414f6fadd34cb5
Luke Bogdanovic <19232691@studentmail.ul.ie>
2022-03-25 00:01:47 +0000

    CS4116-69 Updating api function to use one SQL query

--- api/Connections/connections.php
+++ api/Connections/connections.php
@@ -19,18 +19,18 @@ function get_Connected_Users($id)
     $userIDs = [];
     if ($_SERVER['REQUEST_METHOD'] == "POST") {
         // Getting all users with connection to the user that is logged in
-        $stmt = "SELECT user.UserID, user.Username, user.Firstname, user.Surname, user.DateOfBirth, connections.ConnectionDate FROM connections INNER JOIN user ON connections.userID2=user.UserID WHERE connections.userID1 = $id;";
+        $stmt = "SELECT user.UserID, user.Username, user.Firstname, user.Surname, user.DateOfBirth, connections.ConnectionDate, profile.Description FROM connections INNER JOIN user ON connections.userID2=user.UserID INNER JOIN profile ON profile.UserID=user.userID WHERE connections.userID1 = $id;";
         if ($stmt = mysqli_prepare($con, $stmt)) {
             if (mysqli_stmt_execute($stmt)) {
                 mysqli_stmt_store_result($stmt);
-                mysqli_stmt_bind_result($stmt, $userID, $username, $firstname, $surname, $dob, $connectionDate);
+                mysqli_stmt_bind_result($stmt, $userID, $username, $firstname, $surname, $dob, $connectionDate, $description);
                 if (mysqli_stmt_num_rows($stmt) > 0) {
                     $result = array('status' => 200, 'message' => 'Users connected to logged in user found');
                     // Put all retrieved UserIDs into results array
                     while (mysqli_stmt_fetch($stmt)) {
                         // Changing dob to age and putting in array
                         $age = get_age($dob);
-                        $user = array('userID' => $userID, 'username' => $username, 'firstname' => $firstname, 'surname' => $surname, 'age' => $age, 'connectionDate' => $connectionDate);
+                        $user = array('userID' => $userID, 'username' => $username, 'firstname' => $firstname, 'surname' => $surname, 'age' => $age, 'connectionDate' => $connectionDate, 'description' => $description);
                         array_push($results, $user);
                         array_push($userIDs, $userID);
                     }
@@ -38,31 +38,9 @@ function get_Connected_Users($id)
                 } else {
                     $result = array('status' => 403, 'message' => 'No Users found connected to the logged in user');
                 }
+                echo json_encode($result);
+                return;
             }
         }
-        // Getting the description from profile table
-        // Must be better way to do this
-        for ($i = 0; $i < count($userIDs); $i++) {
-            $stmt = "SELECT profile.Description FROM profile WHERE profile.UserID = ?";
-            if ($stmt = mysqli_prepare($con, $stmt)) {
-                mysqli_stmt_bind_param($stmt, "s", $userIDs[$i]);
-                if (mysqli_stmt_execute($stmt)) {
-                    mysqli_stmt_store_result($stmt);
-                    mysqli_stmt_bind_result($stmt, $description);
-                    if (mysqli_stmt_num_rows($stmt) > 0) {
-                        $result['status'] = 200;
-                        $result['message'] = "Users connected to logged in user found";
-                        while (mysqli_stmt_fetch($stmt)) {
-                            // Inserting description into current user array for js dom insertion
-                            $result[0][$i]['description'] = $description;
-                        }
-                    } else {
-                        $result = array('status' => 403, 'message' => 'No Users found connected to the logged in user');
-                    }
-                }
-            }
-        }
-        echo json_encode($result);
-        return;
     }
 }
