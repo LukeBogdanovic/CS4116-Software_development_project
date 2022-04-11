501788ee31a2bd3e4780cf465f1e0f292da6c959
Luke Bogdanovic <19232691@studentmail.ul.ie>
2022-04-12 00:28:15 +0100

    CS4116-31 Liking and connecting JS and PHP

--- js/search.js
+++ js/search.js
@@ -16,7 +16,10 @@ function getSearchResults(event) {
   $.ajax({
     method: "POST",
     url: "../api/Search/getSearchResult.php",
-    data: { function: "get_Search_result", search: searchTerm },
+    data: {
+      function: "get_Search_result",
+      search: searchTerm,
+    },
     success: (response) => {
       let data = JSON.parse(response);
       if (data.status == 200) {
@@ -58,10 +61,20 @@ function addUserCards(data) {
     const username = card.querySelector("[data-username]");
     const age = card.querySelector("[data-age]");
     const body = card.querySelector("[data-body]");
+    const userID = card.querySelector("[data-userid]");
+    const userLike = card.querySelector("[data-like]");
+    const userProfile = card.querySelector("[data-profile]");
     header.textContent = `${user.firstname} ${user.surname}`;
     username.textContent = user.username;
     age.textContent = user.age;
     body.textContent = user.description;
+    userID.setAttribute("value", user.userID);
+    userLike.setAttribute("onclick", `likeUser(event,${user.userID});`);
+    userLike.setAttribute("id", `user${user.userID}`);
+    userProfile.setAttribute(
+      "onclick",
+      `viewUserProfile(event,${user.userID});`
+    );
     userCardContainer.append(card);
   });
 }
@@ -72,3 +85,65 @@ search.on("keyup", () => {
   clearTimeout(typingTimer);
   typingTimer = setTimeout(getSearchResults, interval);
 });
+
+/**
+ *
+ * @param {Event} event
+ */
+function likeUser(event, userID2) {
+  event.preventDefault();
+  const userID1 = $("#userID").val();
+  $.ajax({
+    method: "POST",
+    url: "../api/Connections/connections.php",
+    data: {
+      function: "like_user",
+      userID1: userID1,
+      userID2: userID2,
+    },
+    success: (response) => {
+      let data = JSON.parse(response);
+      if (data.status == 200) {
+        document
+          .getElementById(`user${userID2}`)
+          .classList.replace("btn-danger", "btn-success");
+        document.getElementById(`user${userID2}`).innerHTML = "User Liked";
+        document.getElementById(`user${userID2}`).setAttribute("disabled", "");
+        window.setTimeout(() => {
+          document
+            .getElementById(`user${userID2}`)
+            .classList.replace("btn-success", "btn-danger");
+          document.getElementById(`user${userID2}`).innerHTML = "Like User";
+          document.getElementById(`user${userID2}`).removeAttribute("disabled");
+        }, 2500);
+        checkUserConnection(userID2);
+      } else {
+        document.getElementById(`user${userID2}`).innerHTML = "Try Again Later";
+        document.getElementById(`user${userID2}`).setAttribute("disabled", "");
+        window.setTimeout(() => {
+          document.getElementById(`user${userID2}`).innerHTML = "Like User";
+          document.getElementById(`user${userID2}`).removeAttribute("disabled");
+        }, 2500);
+      }
+    },
+  });
+}
+
+function checkUserConnection(userID2) {
+  const userID1 = $("#userID").val();
+  $.ajax({
+    method: "POST",
+    url: "../api/Connections/connections.php",
+    data: {
+      function: "check_connection",
+      userID1: userID1,
+      userID2: userID2,
+    },
+    success: (response) => {
+      let data = JSON.parse(response);
+      if (data.status == 200) {
+      } else {
+      }
+    },
+  });
+}
